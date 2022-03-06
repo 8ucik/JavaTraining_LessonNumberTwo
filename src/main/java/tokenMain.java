d7259fd996c9637340ce1c319dbe5be3c7701dea
tb <buciortomasz@gmail.com>
2022-03-06 12:51:07 +0100

    Fixed indentions, code refactor.

--- src/main/java/tokenMain.java
+++ src/main/java/tokenMain.java
@@ -10,38 +10,41 @@ public class tokenMain {
 
             int selection;
             Scanner input = new Scanner(System.in);
-            if (input.hasNextInt()) {
-                selection = input.nextInt();
-            } else {
+            if (!input.hasNextInt()) {
                 System.out.println("Sorry. That is not a number.");
                 return;
             }
+            selection = input.nextInt();
 
-            switch (selection) {
+        switch (selection)
+        {
+            case 0:
+                System.out.println("Bye Bye.");
+                break;
 
-                case 0:
-                    System.out.println("Bye.");
-                    break;
+            case 1:
+                System.out.println(generator.returnText("5")
+                        + "\n"
+                        + generator.getTokenBy(5));
+                break;
 
-                case 1:
-                    System.out.println(generator.returnText("5"));
-                    System.out.println(generator.getTokenBy(5));
-                    break;
+            case 2:
+                System.out.println(generator.returnText("10")
+                        + "\n"
+                        + generator.getTokenBy(10));
+                break;
 
-                case 2:
-                    System.out.println(generator.returnText("10"));
-                    System.out.println(generator.getTokenBy(10));
-                    break;
+            case 3:
+                System.out.println(generator.returnText("15")
+                        + "\n"
+                        + generator.getTokenBy(15));
+                break;
 
-                case 3:
-                    System.out.println(generator.returnText("15"));
-                    System.out.println(generator.getTokenBy(15));
-                    break;
-
-                default:
-                    throw new IllegalStateException("Value unsupported by design. Sorry: "
-                            + input
-                            + " is not accepted.");
-            }
+            default:
+                System.out.println("The entered value ("
+                        + input
+                        + ") is unsupported by design.");
+                break;
+        }
     }
 }
